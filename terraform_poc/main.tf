115f9e398916dfad1a1e79f4929a835a335c1bd1
qachana <archanajjoshi@gmail.com>
2026-02-16 11:00:14 -0400

    working ec2 cluster

--- terraform_poc/main.tf
+++ terraform_poc/main.tf
@@ -9,11 +9,9 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAWNVSY3UV3JZORW5O"
-  secret_key = "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"
-}
+  }
 
 resource "aws_instance" "my_first_server" {
-  ami           = "ami-0071c8c431eea0edb"
+  ami           = "ami-0b6c6ebed2801a5cb"
   instance_type = "t3.micro"
 }
